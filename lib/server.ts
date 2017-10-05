c5ac9ec829ae051d8506d1de01c10d10f8933e07
Garrett Levine <garrlevine@gmail.com>
2017-10-05 13:58:17 -0400

    added redis maybe

--- lib/server.ts
+++ lib/server.ts
@@ -6,6 +6,8 @@ import * as path from 'path';
 import { Request, Response } from 'express';
 
 const router = require('./router');
+const session = require('./session');
+
 const app = express();
 
 // passport configuration
@@ -16,10 +18,12 @@ app.set('trust proxy', 1);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(session.getExpressMW());
+
 app.use(express.static(`${__dirname}/..`));
 app.use('/', router);
 
 app.listen('3000', () => {
     console.log(('App is running at http://localhost:3000 in dev mode'));
-    console.log('Press CTRL-C to stop\n');
+    console.log('Press CTRL-C to stop \n');
 });
