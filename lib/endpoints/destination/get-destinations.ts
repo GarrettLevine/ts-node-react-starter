08fd6355f3a3e42ed4fa72a8a619add21f01b121
Garrett Levine <garrlevine@gmail.com>
2017-10-05 18:52:27 -0400

    cleanup router

---
+++ lib/endpoints/destination/get-destinations.ts
@@ -0,0 +1,2 @@
+import * as Express from 'express';
+
--- lib/router/index.ts
+++ lib/router/index.ts
@@ -1,12 +1,10 @@
 import * as Express from 'express';
-
 const path = require('path');
+
 const router = Express.Router();
+const apiRouter = require('./api-router');
 
-const getUser = require('./get-user');
-
-router.get('/api/user', getUser);
-
+router.use('/api', apiRouter);
 router.use('/*', (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
     res.sendFile(path.resolve('public/index.html'), undefined, err => {
         if (err) next(err);
