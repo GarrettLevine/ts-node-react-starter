90a150f427955b40cd6c249ece0f1999e2151244
Garrett Levine <garrlevine@gmail.com>
2017-10-06 10:25:06 -0400

    PROGESS

--- lib/endpoints/destination/get-destinations.ts
+++ lib/endpoints/destination/get-destinations.ts
@@ -1,15 +1,21 @@
 import * as Express from 'express';
+const JSForce = require('jsforce');
 
-const salesforce = require('node-salesforce');
+const config = require('../../config');
 
-const auth = new salesforce.OAuth2({
-    clientId : '3MVG91ftikjGaMd.gNEtlhbu4kGmjqca97QILKk7l_hAZpzaUp0y2JojnxWOftpulAISLNnKDzmduRpqXulu.',
-    clientSecret : '3130318372906603679',
-    redirectUri : 'http://localhost:3000/salesforce/callback',
-});
+const conn = new JSForce.Connection();
 
+console.log(config.salesforce.username, `${config.salesforce.password}${config.salesforce.securityToken}`);
 function getDestinations(req: Express.Request, res: Express.Response) {
-    res.redirect(auth.getAuthorizationUrl({ scope: 'api id web' }));
+    const promise = new Promise((resolve: any, reject: any) => {
+        conn.login(config.salesforce.username, `${config.salesforce.password}${config.salesforce.securityToken}`, (err: any, userInfo: object) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+        });
+        resolve(true);
+    });
 }
 
 module.exports = getDestinations;
