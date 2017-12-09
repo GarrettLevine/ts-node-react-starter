23d95d61871eeef8f74f47527ea1a610bbffe715
Garrett Levine <garrlevine@gmail.com>
2017-12-09 11:05:44 -0500

    fixes routing with react router

--- lib/router/index.ts
+++ lib/router/index.ts
@@ -3,11 +3,12 @@ import * as Express from 'express';
 const path = require('path');
 const router = Express.Router();
 
-router.use('/*', (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
-    res.sendFile(path.resolve('public/index.html'), undefined, err => {
-        if (err) next(err);
-    });
-    next();
+router.get('/api', (req, res, next) => {
+    res.status(200).json({ great: 'great' });
+});
+
+router.get('*', (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+    res.sendFile(path.resolve('public/index.html'));
 });
 
 module.exports = router;
